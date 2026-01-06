5b437202dc806984696ef4ed6a83498313ac23f5
Adithya Krishna <118156633+AK-ADITHYA@users.noreply.github.com>
2026-01-06 23:44:35 +0530

    frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://sharespace-backend-xh5c.onrender.com';
 
 // --- 1. Export authAPI (Fixes your error) ---
 export const authAPI = {
